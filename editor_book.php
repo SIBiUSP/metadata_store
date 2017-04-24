37514d2f26e79fe0808d4ddafa29465b2556465e
Tiago Murakami <trmurakami@gmail.com>
2017-04-24 14:55:36 -0300

    Titulo

--- editor_book.php
+++ editor_book.php
@@ -9,8 +9,10 @@
 	</head>	
 	<body>
 		<div class="uk-container uk-container-center">
+			<h1>Editor de metadados</h1>
 			<form class="uk-form-horizontal uk-margin-large" method="post" action="editor.php">
 				<?php form::read_json(); ?>
+				<button class="uk-button uk-button-primary uk-width-1-1 uk-margin-small-bottom">Salvar</button>
 			</form>	
 		</div>
 		<script>
