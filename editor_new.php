9cab98609ed296fa29178adabb5fec1bd05179ba
Tiago Murakami <trmurakami@gmail.com>
2017-04-26 07:47:40 -0300

    Melhorias gerais

--- editor_new.php
+++ editor_new.php
@@ -30,7 +30,7 @@
 				var wrapper         = $(".input_fields_wrap"); //Fields wrapper
 				var add_button      = $(".add_field_button"); //Add button ID
 				var x = 1; //initlal text box count
-				$(add_button).click(function(e){ //on add input button click
+				$(add_button).on("click", function(e){ //on add input button click
 					e.preventDefault();
 					if(x < max_fields){ //max input box allowed
 						x++; //text box increment
@@ -39,11 +39,11 @@
 						var parentElement =  copyElement.parentElement;
 						var newdiv = document.createElement('div');
 						newdiv.className += "input_fields_wrap";
-						newdiv.innerHTML = copyElement.innerHTML;
+						newdiv.innerHTML = copyElement.innerHTML;						
 						parentElement.append(newdiv); //add input box
 					}
 				});
-				$(wrapper).on("click",".remove_field", function(e){ //user click on remove text
+				$('body').on("click",".remove_field", function(e){ //user click on remove text
 					e.preventDefault(); this.parentElement.remove(); x--;
 				})
 			});
